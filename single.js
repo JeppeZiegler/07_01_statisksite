14eeeecbf1493d9d70bc3b84af360104f3584a0d
Jeppe Ziegler <jezi0001@stud.kea.dk>
2025-01-31 00:04:22 +0100

    feature dynamisk single

--- single.js
+++ single.js
@@ -1,20 +1,24 @@
-let productId = 1165;
-let productContainer = document.querySelector(".productContainer");
+const productContainer = document.querySelector(".productContainer");
+const productId = 1164;
 fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
   .then((response) => response.json())
   .then((data) => {
     productContainer.innerHTML = `
-     <div class="product_site_grid">
-                <div><img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="Productbillede" class="productImage></div>
-                <div>
+    
+    <figure>
+                <img src="https://kea-alt-del.dk/t7/images/webp/640/${data.productId}.webp" alt="Produktbillede" class="productImage/>
+                <img src="img/sahara.png" alt="Sahara">
+                </figure>
+                <div class="productDetails">
                     <h1 class="productname">${data.productdisplayname}</h1>
-                    <h3>Nike | T-shirts</h3>
+                    <h3 class ="brandName">${data.brandname}</h3>
+                    <h3 class ="subCategory">${data.subcategory}</h3>
                     <h5>Color</h5>
-                    <p>Blue</p>
+                    <p class="baseColor">${data.basecolour}</p>
+              
                     <h5>Price</h5>
-                    <p>DKK 1595,-</p>
+                    <p class="price">DKK ${data.price},- </p>
                     <h5>Size</h5>
-
 
                     <select id="size" name="size">
                         <option value="x-small">XS</option>
@@ -27,6 +31,6 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
                     <button class="basket">Add to basket</button>
                 </div>
 
-            </div>
+            
     `;
   });
